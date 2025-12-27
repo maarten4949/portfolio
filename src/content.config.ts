b2e0d831cdd36039bc8d7363a4b3cb3861a17531
maarten4949 <65917042+maarten4949@users.noreply.github.com>
2025-12-27 14:13:18 +0100

    feat: added starter project and added collection for projects

---
+++ src/content.config.ts
@@ -0,0 +1,44 @@
+// 1. Import utilities from `astro:content`
+import { defineCollection, reference } from "astro:content";
+
+// 2. Import loader(s)
+import { glob, file } from "astro/loaders";
+
+// 3. Import Zod
+import { z } from "astro/zod";
+
+// 4. Define your collection(s)
+const projects = defineCollection({
+  loader: glob({ pattern: "**/*.md", base: "./src/data/projects" }),
+  schema: ({ image }) =>
+    z.object({
+      title: z.string().min(1).max(100),
+      description: z.string().min(1).max(500),
+      longDescription: z.string().min(1).max(5000),
+      image: image(),
+      tags: z.array(z.string().min(1).max(100)).min(1).max(10),
+      links: z
+        .array(
+          z.object({
+            url: z.string().url().min(1).max(200),
+            label: z.string().min(1).max(100),
+          }),
+        )
+        .min(1)
+        .max(10),
+    }),
+});
+const technologies = defineCollection({
+  loader: glob({ pattern: "**/*.md", base: "./src/data/technologies" }),
+  schema: ({ image }) =>
+    z.object({
+      title: z.string().min(1).max(100),
+      description: z.string().min(1).max(500),
+      experience: z.string().min(1).max(5000),
+      projects: z.array(reference("projects")),
+      image: image(),
+    }),
+});
+
+// 5. Export a single `collections` object to register your collection(s)
+export const collections = { projects, technologies };
---
+++ src/data/projects/allertourer.md
@@ -0,0 +1,9 @@
+---
+title: Aller Tourner
+slug: allertourner
+description: An e-commerce site for selling pottery workshops.
+longDescription: This is an e-commerce site that started as a small two-person assignment in high school (basic CRUD website, only adding, updating, inserting and deleting products as an admin was required). My teammate wanted to use it in production, but wasn’t that interested in programming herself, so I continued working on the site after high school, adding new features and improving the webshop as a freelance developer. I made the entire backend in php, I also did all the frontend javascript and about 80% of frontend html and css.
+links: [{url: "https://allertourner.be/", label: "Visit Site"}]
+tags: ["PHP", "Vanilla JS", "MySQL", "In Production", "Client Work"]
+image: "../../assets/images/allertourner.png"
+---
